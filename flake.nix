047019b9249dbcc56750ce7ef8f64ada7cca688a
Ruaidhri Devery <deveryr2@gmail.com>
2023-05-18 13:47:55 -0700

    Fix flake validation errors
    Replace deprecated defaultPackages output attribute
    as requested by the nix flake validate command.

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+    packages.x86_64-linux.default = home-manager.defaultPackage.x86_64-linux;
+    packages.x86_64-darwin.default = home-manager.defaultPackage.x86_64-darwin;
 
     homeConfigurations = {
       "ruaidhridevery@chromebook" = home-manager.lib.homeManagerConfiguration {
